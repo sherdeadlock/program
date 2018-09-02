5761d530ceee437865ac530df2ce5b2f927e3039
neil <sherdeadlock@gmail.com>
2018-09-02 20:51:54 +0800

    extjs: treegrid resize column automatically

--- extjs/tree/treegrid.js
+++ extjs/tree/treegrid.js
@@ -40,7 +40,8 @@ const TreeStore = function TreeStore() {
 
     this.groupByUidModular = (users) => {
         let groups = {};
-        for (let i = 0, len = users.length; i < len; i++) {
+		var max = Math.min(20, users.length);
+        for (let i = 0, len = max; i < len; i++) {
             let user = users[i];
             user.leaf = true;
             user.uid = user.id;
@@ -74,7 +75,47 @@ const TreeStore = function TreeStore() {
 
 let store = new TreeStore();
 
-let tree = new Ext.ux.tree.TreeGrid({
+const MyTree = Ext.extend(Ext.ux.tree.TreeGrid, {
+	updateColumnWidths2: function() {
+		console.log('width', this.getWidth());
+		console.log('innerWidth', this.getInnerWidth());
+		console.log('frameWidth', this.getFrameWidth());
+		console.log('height', this.getHeight());
+		console.log('innerHeight', this.getInnerHeight());
+		console.log('frameHeight', this.getFrameHeight());
+		console.log(this.mainHd);
+		console.log(this.innerHd);
+		console.log(this.scrollOffset);
+		console.log(this.innerBody, this.innerBody.dom.scrollHeight, this.innerBody.dom.clientHeight);
+		console.log('innerCt', this.innerCt, this.innerCt.dom.scrollHeight, this.innerCt.dom.clientHeight);
+
+		var hasVScrollBar = this.innerBody.dom.scrollHeight > this.innerBody.dom.clientHeight;
+		var cs = this.columns.filter(function(c) { return !c.hidden; });
+		var TableMaxWidth = this.getWidth() - this.scrollOffset;
+		console.log('mainHd width', this.mainHd.getWidth());
+		console.log('innerHd width', this.innerHd.getWidth());
+		console.log('scrollOffset', this.scrollOffset);
+		console.log('TableMaxWidth', TableMaxWidth);
+
+		var totalScore = cs.reduce(function(sum, c) { return sum + c.width; }, 0);
+		var widthSum = 0;
+		cs.forEach(function(c) {
+			var score = c.width;
+			var width = Math.floor(TableMaxWidth * score / totalScore);
+			widthSum += width;
+			c.width = width;
+		});
+		if (!hasVScrollBar && this.scrollOffset) {
+			cs[cs.length - 1].width += (this.scrollOffset - 2);
+		}
+
+		console.log(cs);
+
+		MyTree.superclass.updateColumnWidths.call(this);
+	}
+});
+
+let tree = new MyTree({
     tbar: new Ext.Toolbar(),
     store,
     rootVisible: false,
@@ -92,6 +133,8 @@ let tree = new Ext.ux.tree.TreeGrid({
             dataIndex: 'money',
             width: 200,
             sortable: false,
+			minWidth: 300,
+			maxWidth: 400,
         },
         {
             header: 'Active',
---
+++ unix/linux.md
@@ -0,0 +1,3 @@
+* https://matt.sh/howto-c
+* http://icube-icps.unistra.fr/img_auth.php/d/db/ModernC.pdf
+* https://www.gnu.org/software/libc/manual/html_node/index.html glibc
